7d55f5bf44427a4a94b76e43228c297335a6bb9d
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-10 20:47:36 +0200

    fix: click outside to close mobile menu

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,11 +1,33 @@
 "use client";
 import { LocateFixed, Menu, X } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { links } from "../lib/links";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+  const iconRef = useRef<HTMLDivElement | null>(null);
+
+  // handle click outside to close the menu
+  const handleClickOutside = (e: MouseEvent) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(e.target as Node) &&
+      iconRef.current &&
+      !iconRef.current.contains(e.target as Node)
+    ) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div>
@@ -18,8 +40,12 @@ export default function Navbar() {
           </h1>
         </Link>
 
-        {/* menu */}
-        <div onClick={() => setMenuOpen(!menuOpen)} className=" cursor-pointer">
+        {/* menu icon */}
+        <div
+          ref={iconRef}
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="cursor-pointer"
+        >
           {menuOpen ? (
             <X className="text-gray-700" height={25} width={25} />
           ) : (
@@ -32,9 +58,10 @@ export default function Navbar() {
 
       {/* the links on mobile */}
       <div
-        className={`
-                ${menuOpen ? "top-16" : "-top-full"}
-                  m-2 flex flex-col gap-2 font-semibold text-gray-600 absolute w-[97%] transition-all duration-700  bg-white`}
+        ref={menuRef}
+        className={`${
+          menuOpen ? "top-16" : "-top-full"
+        } m-2 flex flex-col gap-2 font-semibold text-gray-600 absolute w-[97%] transition-all duration-700 bg-white`}
       >
         {links.map((li) => (
           <Link
