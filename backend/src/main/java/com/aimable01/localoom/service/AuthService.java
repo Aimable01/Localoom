d7c9ab9bec808bad303d7619549b39205015d900
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-15 13:54:59 +0200

    feat: java mail sender

---
+++ backend/src/main/java/com/aimable01/localoom/service/AuthService.java
@@ -0,0 +1,8 @@
+package com.aimable01.localoom.service;
+
+import com.aimable01.localoom.security.JwtTokenProvider;
+
+public class AuthService {
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+}
---
+++ backend/src/main/java/com/aimable01/localoom/service/EmailService.java
@@ -0,0 +1,22 @@
+package com.aimable01.localoom.service;
+
+import com.aimable01.localoom.model.User;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendVerificationEmail(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmail());
+        message.setSubject("Verification Email");
+        message.setText("Your verification code is: " + user.getVerificationCode());
+        mailSender.send(message);
+    }
+}
